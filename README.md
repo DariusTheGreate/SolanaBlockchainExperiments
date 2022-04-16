a9d21181c7005853376ad494a8a328be328a6a38
DariusTheGreate <kotovshikov31415926@gmail.com>
2022-04-17 00:53:40 +0500

    quick start

---
+++ README.md
@@ -0,0 +1,7 @@
+# Solana rust programming
+
+#### Some solana rust programming. Note that Solana is looks like a new EOS, and might be a scum. In which all of the tokens was bought by whales, and right now being sold to hamsters with 100x profit.
+#### And rust is looks like a possible replacement for c++. And can easily become standart language for blockchain applications.
+
+![output example](/assets/blockchain.png "blockchain")
+![output example](/assets/offchain.png "offchain")
---
+++ client/main.ts
@@ -0,0 +1,31 @@
+import {
+  establishConnection,
+  establishPayer,
+  checkProgram,
+  sayHello,
+  reportGreetings,
+} from './hello_world';
+
+async function main() {
+  console.log("Let's say hello to a Solana account...");
+
+  await establishConnection();
+
+  await establishPayer();
+
+  await checkProgram();
+
+  await sayHello();
+
+  await reportGreetings();
+
+  console.log('Success');
+}
+
+main().then(
+  () => process.exit(),
+  err => {
+    console.error(err);
+    process.exit(-1);
+  },
+);
